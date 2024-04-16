7503185a2e96954b113415e68d98c4c8855f1766
Ben Pocklington <rabidmexican@gmail.com>
2024-04-16 11:29:09 +0200

    fix: fixed alignment in Game scene

--- src/game/scenes/game/Game.js
+++ src/game/scenes/game/Game.js
@@ -5,21 +5,29 @@ import { EVENTS, EventBus } from '../../EventBus';
 export class Game extends Phaser.Scene {
   buttons = {};
 
+  width;
+  height;
+
   constructor() {
     super('Game');
   }
 
   create() {
+    // get width and height
+    this.width = this.sys.game.canvas.width;
+    this.height = this.sys.game.canvas.height;
+    const centerX = this.width / 2;
+    const centerY = this.height / 2;
+
     // add background color
     this.cameras.main.setBackgroundColor(0x00ff00);
 
     // add background image
     this.add.image(512, 384, 'background').setAlpha(0.5);
 
-    this.add.text(512, 384, 'INSERT ACTUAL GAME HERE', {
-      fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+    this.add.text(centerX, centerY, 'INSERT ACTUAL GAME HERE', {
+      fontFamily: 'Arial Black', fontSize: 24, color: '#ffffff',
       stroke: '#000000', strokeThickness: 8,
-      align: 'center'
     }).setOrigin(0.5).setDepth(100);
 
     EventBus.emit(EVENTS.CURRENT_SCENE_READY, this);
